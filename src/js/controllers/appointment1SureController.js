65057e478715029fa310169d17d9e7512bdaedc0
XiaoMingLi <1165406082@qq.com>
2016-06-08 14:29:12 +0800

    modified input-group

--- src/js/controllers/appointment1SureController.js
+++ src/js/controllers/appointment1SureController.js
@@ -1,3 +1,15 @@
 angular.module("Love").controller("appointment1SureController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+    $scope.modal = {
+        status: 0
+    };
+    $scope.info = "双方确认赴约后，保证金将退回";
+    $scope.open_modal = function() {
+        $scope.modal.status = 1;
+    }
+    $scope.cancel_modal = function() {
+        $scope.modal.status = 0;
+    }
+    $scope.confirm_modal = function() {
+        $scope.modal.status = 0;
+    }
 })
