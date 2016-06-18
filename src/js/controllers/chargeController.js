574c0e333b3cbb701e561b6318d07a3378528c80
XiaoMingLi <1165406082@qq.com>
2016-06-18 10:13:50 +0800

    modified

--- src/js/controllers/chargeController.js
+++ src/js/controllers/chargeController.js
@@ -1,28 +1,30 @@
 angular.module("Love").controller("chargeController", function($scope, errorServices, toastServices, localStorageService, config) {
     console.log("dd")
-	$scope.step=1;
-	$scope.show_step=function(step){
-		$scope.step= step;
-	}
-	
-	$scope.charges = [{
-		name:"1个月VIP",
-		money:"X元",
-		select:false
-	},{
-		name:"3个月VIP",
-		money:"X元",
-		select:false
-	},{
-		name:"4个月VIP",
-		money:"X元",
-		select:false
-	},{
-		name:"12个月VIP",
-		money:"X元",
-		select:false
-	}];
-	$scope.select = function(charge){
-		charge.select= !charge.select;
-	}
+    $scope.step = 1;
+    $scope.show_step = function(step) {
+        $scope.step = step;
+    }
+
+    $scope.charges = [{
+        name: "1个月VIP",
+        money: "X元",
+        select: false
+    }, {
+        name: "3个月VIP",
+        money: "X元",
+        select: false
+    }, {
+        name: "4个月VIP",
+        money: "X元",
+        select: false
+    }, {
+        name: "12个月VIP",
+        money: "X元",
+        select: false
+    }];
+    $scope.select = function(charge) {
+        charge.select = !charge.select;
+    }
+    $scope.number = "2400";
+    
 })
