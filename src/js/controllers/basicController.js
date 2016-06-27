355475c3eade5bac2bf95f10ddabc4e95aa57f61
XiaoMingLi <1165406082@qq.com>
2016-06-27 16:35:46 +0800

    authen

--- src/js/controllers/basicController.js
+++ src/js/controllers/basicController.js
@@ -295,6 +295,19 @@ angular.module("Love").controller("basicController", function($scope, $timeout, 
 			}
 		})
 	}
+
+	$scope.save= function() {
+		userServices.save({
+
+		}).then(function(data) {
+			toastServices.hide()
+			if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
+				errorServices.autoHide(data.message);
+			} else {
+				errorServices.autoHide(data.message);
+			}
+		})
+	}
 });
 // uploadCoversController
 angular.module("Love").controller("uploadCoversController", function($scope, errorServices, toastServices, localStorageService, config) {
--- src/js/controllers/messagesController.js
+++ src/js/controllers/messagesController.js
@@ -1,3 +1,60 @@
-angular.module("Love").controller("messagesController", function($scope, errorServices, toastServices, localStorageService, config) {
+angular.module("Love").controller("messagesController", function($scope, $location, userServices, errorServices, toastServices, localStorageService, config) {
+    $scope.news = [];
+    $scope.modal = {}
+    $scope.page = {
+        pn: 1,
+        page_size: 5,
+        message: "点击加载更多"
+    }
+    $scope.loadMore = function() {
+        if ($scope.no_more) {
+            return;
+        }
+        toastServices.show();
+        $scope.page.message = "正在加载...";
+        userServices.query_messages($scope.page).then(function(data) {
+            toastServices.hide();
+            $scope.page.message = "点击加载更多";
+            if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
+                $scope.news = $scope.news.concat(data.Result.CollectUsers.list);
+                $scope.no_more = $scope.news.length == data.Result.CollectUsers.totalRow ? true : false;
+            } else {
+                $scope.page.message = "加载完成，共加载" + $scope.news.length + "条记录";
+            }
+            if ($scope.no_more) {
+                $scope.page.message = "没有了";
+            }
+            $scope.page.pn++;
+        })
 
+    }
+    $scope.loadMore();
+
+    userServices.query_basicinfo().then(function(data) {
+
+        if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
+            $scope.user = data.Result.BaseInfo;
+        } else {
+            errorServices.autoHide(data.message);
+        }
+    });
+
+    $scope.go = function(id) {
+        if ($scope.user.is_vip == '1') {
+            $location.path("chat").search({
+                id: id
+            })
+        } else {
+
+            $scope.modal.status = 1
+
+        }
+
+    };
+
+    $scope.confirm_modal = function() {
+        $location.path("charge").search({
+            
+        })
+    }
 })
