6d879b60abfc45753fc331ad142250549400b777
dribehance <dom4j.for.networking@gmail.com>
2016-06-27 16:33:24 +0800

    fix webkit-select input ,textarea bug

--- src/js/controllers/manualAuthenController.js
+++ src/js/controllers/manualAuthenController.js
@@ -1,4 +1,13 @@
 // by dribehance <dribehance.kksdapp.com>
-angular.module("Love").controller("manualAuthenController", function($scope, errorServices, toastServices, localStorageService, config) {
-
+angular.module("Love").controller("manualAuthenController", function($scope, userServices, errorServices, toastServices, localStorageService, config) {
+	// 投诉信息
+	toastServices.show();
+	userServices.query_report_info().then(function(data) {
+		toastServices.hide()
+		if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
+			$scope.report_info = data.Result.Constant;
+		} else {
+			errorServices.autoHide(data.message);
+		}
+	})
 })
