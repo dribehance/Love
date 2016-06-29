cd5a44fc36344f9a42512ec00f892ffb5925e69b
dribehance <dom4j.for.networking@gmail.com>
2016-06-29 14:23:19 +0800

    fix basic

--- src/js/controllers/basicController.js
+++ src/js/controllers/basicController.js
@@ -404,19 +404,6 @@ angular.module("Love").controller("basicController", function($scope, $filter, $
 			}
 		})
 	}
-
-	$scope.save = function() {
-		userServices.save({
-
-		}).then(function(data) {
-			toastServices.hide()
-			if (data.code == config.request.SUCCESS && data.status == config.response.SUCCESS) {
-				errorServices.autoHide(data.message);
-			} else {
-				errorServices.autoHide(data.message);
-			}
-		})
-	}
 });
 
 
